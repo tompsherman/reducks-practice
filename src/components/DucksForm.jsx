c436de9758a095182c185349418989f9aad31efb
unknown <tompsherman@gmail.com>
2020-10-12 12:01:02 -0400

    step 4: add your recipes

--- src/components/DucksForm.jsx
+++ src/components/DucksForm.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {connect} from 'react-redux'
+import {postDucks} from '../actions'
 
-const DucksForm = () => {
+
+
+const DucksForm = (props) => {
+    const [duck, setDuck] = useState({
+        species: "",
+        region: "",
+        cookTime: ""
+    })
+
+    const submitHandler = event => {
+        event.preventDefault();
+        props.postDucks(duck)
+    }
+
+    const changeHandler = event => {
+        setDuck({...duck, [event.target.name]: event.target.value})
+    }
+
     return (
         <div>
-            
+            <form onSubmit={submitHandler}>
+                <input 
+                    name='species'
+                    type='text'
+                    value={props.species}
+                    onChange={changeHandler}
+                    placeholder='enter species'
+                />
+                <input 
+                    name='region'
+                    type='text'
+                    value={props.region}
+                    onChange={changeHandler}
+                    placeholder='enter region'
+                />
+                <input 
+                    name='cookTime'
+                    type='text'
+                    value={props.cookTime}
+                    onChange={changeHandler}
+                    placeholder='enter cook time'
+                />
+                <button>add that duck!</button>
+            </form>
         </div>
     )
 }
 
-export default DucksForm
+const mapStateToProps = (state) => {
+    return {
+        ducks: state.ducks,
+        isLoading: state.isLoading,
+        error: state.error
+    }
+}
+
+export default connect (mapStateToProps, {postDucks}) (DucksForm)
