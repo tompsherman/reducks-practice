bf321c3a244802e5bff3f9ee2bd6826cae568b8b
unknown <tompsherman@gmail.com>
2020-10-12 10:44:16 -0400

    step 2: import relatives (dependencies) and have provider go to the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import {thunk} from 'redux-thunk'
+import reducer from './reducers.jsx'
+
 import './index.css'
+
 import App from './App'
+
+const store = createStore(reducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store= {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
