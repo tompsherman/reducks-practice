9cd8ff4df5760d1bb22d42d8c391559af33b6701
unknown <tompsherman@gmail.com>
2020-10-12 14:52:18 -0400

    step 4: duck form

--- src/components/DucksForm.jsx
+++ src/components/DucksForm.jsx
@@ -59,3 +59,7 @@ const mapStateToProps = (state) => {
 }
 
 export default connect (mapStateToProps, {postDucks}) (DucksForm)
+
+//-------------------------------------\\
+
+// "import React, {useState} from 'react'\nimport {connect} from 'react-redux'\nimport {postDucks} from '../actions'\n\nconst DucksForm = (props) => {\n\tconst [duck, setDuck] = useState({\n\t\tspecies: '',\n\t\tregion: '',\n\t\tcookTime: ''\n\t})\n\n\tconst submitHandler = event => {\n\t\tevent.preventDefault();\n\t\tprops.postDucks(duck)\n\t}\n\n\tconst changeHandler = event => {\n\t\tsetDuck({...duck, [event.target.name]: event.target.value})\n\t}\n\n\treturn (\n\t\t<div>\n\t\t\t<form onSubmit={submitHandler}>\n\t\t\t\t<input\n\t\t\t\t\tname='species'\n\t\t\t\t\ttype='text'\n\t\t\t\t\tvalue={props.species}\n\t\t\t\t\tonChange={changeHandler}\n\t\t\t\t\tplaceholder='enter species'\n\t\t\t\t/>\n\t\t\t\t<input\n\t\t\t\t\tname='region'\n\t\t\t\t\ttype='text'\n\t\t\t\t\tvalue={props.region}\n\t\t\t\t\tonChange={changeHandler}\n\t\t\t\t\tplaceholder='enter region'\n\t\t\t\t/>\n\t\t\t\t<input\n\t\t\t\t\tname='cookTime'\n\t\t\t\t\ttype='text'\n\t\t\t\t\tvalue={props.cookTime}\n\t\t\t\t\tonChange={changeHandler}\n\t\t\t\t\tplaceholder='enter cook time'\n\t\t\t\t/>\n\t\t\t\t<button>add that duck!</button>\n\t\t\t</form>\n\t\t</div>\n\t)\n}\n\nconst mapStateToProps = (state) => {\n\treturn {\n\t\tducks: state.ducks,\n\t\tisLoading: state.isLoading,\n\t\terror: state.error\n\t}\n}\n\nexport default connect (mapStateToProps, {postDucks}) (DucksForm)"
