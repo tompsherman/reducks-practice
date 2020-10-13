8b7a817d847e414d5662ad58efa762942db6a0f0
unknown <tompsherman@gmail.com>
2020-10-13 09:33:44 -0400

    step 6 or 7: reduce to a boil (start putting ducks in)

--- src/reducers.jsx/index.js
+++ src/reducers.jsx/index.js
@@ -1,16 +1,50 @@
+import { FETCH_DUCKS_START, FETCH_DUCKS_SUCCESS, FETCH_DUCKS_FAILURE, POST_DUCKS_START, POST_DUCKS_SUCCESS, POST_DUCKS_FAILURE } from '../actions'
+
 const initialState = {
-    ducks: "",
+    ducks: '',
     isLoading: false,
-    error: ""
+    error: ''
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
-        case FETCH_START:
+        case FETCH_DUCKS_START:
             return {
                 ...state,
                 isLoading: true,
-                error: ""
+                error: ''
+            }
+        case FETCH_DUCKS_SUCCESS:
+            return {
+                ...state,
+                ducks: action.payload,
+                isLoading: false,
+                error: ''
+            }
+        case FETCH_DUCKS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+        case POST_DUCKS_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            }
+        case POST_DUCKS_SUCCESS:
+            return {
+                ...state,
+                ducks: action.payload,
+                isLoading: false,
+                error: ''
+            }
+        case POST_DUCKS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
             }
         default:
             return state
