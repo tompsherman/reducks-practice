8434edcf4487d10027aff6aaf45999290c9d245e
unknown <tompsherman@gmail.com>
2020-10-16 17:36:17 -0400

    redureduindex

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,24 +26,4 @@ export const postDucks = (duck) => (dispatch) => {
 
 import axios from 'axios'
 
-// "export const FETCH_DUCKS_START = 'FETCH_DUCKS_START'
-// export const FETCH_DUCKS_SUCCESS = 'FETCH_DUCKS_SUCCESS'
-// export const FETCH_DUCKS_FAILURE = 'FETCH_DUCKS_FAILURE'
-// export const fetchDucks = () => (dispatch) => {
-//     dispatch({type: FETCH_DUCKS_START})
-//     axios
-//         .get('https://www.ducks.com')
-//         .then(response => {dispatch({type: FETCH_DUCKS_SUCCESS, payload: response.data})})
-//         .catch(error => dispatch({type: FETCH_DUCKS_FAILURE, payload: error.response}))
-// }
-
-// export const POST_DUCKS_START = 'POST_DUCKS_START'
-// export const POST_DUCKS_SUCCESS = 'POST_DUCKS_SUCCESS'
-// export const POST_DUCKS_FAILURE = 'POST_DUCKS_FAILURE'
-// export const postDucks = (duck) => (dispatch) => {
-//     dispatch({type: POST_DUCKS_START})
-//     axios
-//         .post('https://www.ducks.com', duck)
-//         .then(response => {dispatch({type: POST_DUCKS_SUCCESS, payload: response.data})})
-//         .catch(error => dispatch({type: POST_DUCKS_FAILURE, payload: error.response}))
-// }"
+// "export const FETCH_DUCKS_START = 'FETCH_DUCKS_START'\nexport const FETCH_DUCKS_SUCCESS = 'FETCH_DUCKS_SUCCESS'\nexport const FETCH_DUCKS_FAILURE = 'FETCH_DUCKS_FAILURE'\nexport const fetchDucks = () => (dispatch) => {\n\tdispatch({type: FETCH_DUCKS_START})\n\taxios\n\t\t.get('https://www.ducks.com')\n\t\t.then(response => {dispatch({type: FETCH_DUCKS_SUCCESS, payload: response.data})})/n/t/t.catch(error => dispatch({type: FETCH_DUCKS_FAILURE, payload: error.response}))\n}\n\nexport const POST_DUCKS_START = 'POST_DUCKS_START'\nexport const POST_DUCKS_SUCCESS = 'POST_DUCKS_SUCCESS'\nexport const POST_DUCKS_FAILURE = 'POST_DUCKS_FAILURE'\nexport const postDucks = (duck) => (dispatch) => {\n\tdispatch({type: POST_DUCKS_START})\n\taxios\n\t\t.post('https://www.ducks.com', duck)\n\t\t.then(response => {dispatch({type: POST_DUCKS_SUCCESS, payload: response.data})})\n\t\t.catch(error => dispatch({type: POST_DUCKS_FAILURE, payload: error.response}))\n}"
--- src/reducers.jsx/index.js
+++ src/reducers.jsx/index.js
@@ -57,58 +57,5 @@ export default reducer
 
 // step2 or 3: "const initialState = {\n\tducks: '',\n\tisLoading: false,\n\terror: ''\n}\n\nconst reducer = (state = initialState, action) => {\n\tswitch(action.type){\n\t\tcase FETCH_START:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tisLoading: true,\n\t\t\t\terror: ''\n\t\t\t}\n\t\tdefault:\n\t\t\treturn state\n\t}\n}\n\nexport default reducer"
 
-//step 6 or 7: "import { FETCH_DUCKS_START, FETCH_DUCKS_SUCCESS, FETCH_DUCKS_FAILURE, POST_DUCKS_START, POST_DUCKS_SUCCESS, POST_DUCKS_FAILURE } from '../actions'
-
-// const initialState = {
-//     ducks: '',
-//     isLoading: false,
-//     error: ''
-// }
-
-// const reducer = (state = initialState, action) => {
-//     switch(action.type){
-//         case FETCH_DUCKS_START:
-//             return {
-//                 ...state,
-//                 isLoading: true,
-//                 error: ''
-//             }
-//         case FETCH_DUCKS_SUCCESS:
-//             return {
-//                 ...state,
-//                 ducks: action.payload,
-//                 isLoading: false,
-//                 error: ''
-//             }
-//         case FETCH_DUCKS_FAILURE:
-//             return {
-//                 ...state,
-//                 isLoading: false,
-//                 error: action.payload
-//             }
-//         case POST_DUCKS_START:
-//             return {
-//                 ...state,
-//                 isLoading: true,
-//                 error: ''
-//             }
-//         case POST_DUCKS_SUCCESS:
-//             return {
-//                 ...state,
-//                 ducks: action.payload,
-//                 isLoading: false,
-//                 error: ''
-//             }
-//         case POST_DUCKS_FAILURE:
-//             return {
-//                 ...state,
-//                 isLoading: false,
-//                 error: action.payload
-//             }
-//         default:
-//             return state
-//     }
-// }
-
-// export default reducer"
+//step 6 or 7: "import { FETCH_DUCKS_START, FETCH_DUCKS_SUCCESS, FETCH_DUCKS_FAILURE, POST_DUCKS_START, POST_DUCKS_SUCCESS, POST_DUCKS_FAILURE } from '../actions'\n\nconst initialState = {\n\tducks: '',\n\tisLoading: false,\n\terror: ''\n}\n\nconst reducer = (state = initialState, action) => {\n\tswitch(action.type){\n\t\tcase FETCH_DUCKS_START:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tisLoading: true,\n\t\t\t\terror: ''\n\t\t\t}\n\t\tcase FETCH_DUCKS_SUCCESS:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tducks: action.payload,\n\t\t\t\tisLoading: false,\n\t\t\t\terror: ''\n\t\t\t}\n\t\tcase FETCH_DUCKS_FAILURE:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tisLoading: false,\n\t\t\t\terror: action.payload\n\t\t\t}\n\t\tcase POST_DUCKS_START:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tisLoading: true,\n\t\t\t\terror: ''\n\t\t\t}\n\t\tcase POST_DUCKS_SUCCESS:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tducks: action.payload,\n\t\t\t\tisLoading: false,\n\t\t\t\terror: ''\n\t\t\t}\n\t\tcase POST_DUCKS_FAILURE:\n\t\t\treturn {\n\t\t\t\t...state,\n\t\t\t\tisLoading: false,\n\t\t\t\terror: action.payload\n\t\t\t}\n\t\tdefault:\n\t\t\treturn state\n\t}\n}\nexport default reducer",
 
